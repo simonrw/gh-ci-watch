d6b44c725f08144a9e86e0f70955cfbb8bd92f35
Simon Walker <s.r.walker101@googlemail.com>
2024-09-11 12:57:51 +0100

    Implement in-progress calculation

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,27 @@
+use std::time::Duration;
+
+use clap::Parser;
 use color_eyre::eyre::{self, Context};
 use github::{
-    Commit, GetPullRequestResponse, GetRunJobsResponse, GetWorkflowRunsQueryArgs,
-    GetWorkflowRunsResponse, GitHubClient, RunJob,
+    GetPullRequestResponse, GetRunJobsResponse, GetWorkflowRunsQueryArgs, GetWorkflowRunsResponse,
+    GitHubClient, RunJob,
 };
-use serde::{Deserialize, Serialize};
 
 mod github;
 
 // TODO: how to calculate progress? Is the list of jobs/steps consistent?
 const EXT_TESTS_NUMBER: i64 = 107927392;
 
+#[derive(Parser)]
+struct Args {
+    #[clap(short, long, default_value_t = 10)]
+    sleep_time: u64,
+}
+
 #[derive(Debug, Clone, Copy)]
 enum Status {
     Queued,
+    #[allow(dead_code)]
     InProgress(f32),
     Succeeded,
     Failed,
@@ -44,17 +53,41 @@ impl From<PrDescription> for Pr {
     }
 }
 
+struct PollerBuilder {
+    client: GitHubClient,
+    sleep_time: Option<u64>,
+}
+
+impl PollerBuilder {
+    fn with_sleep_time(mut self, sleep_time: u64) -> Self {
+        self.sleep_time = Some(sleep_time);
+        self
+    }
+
+    fn build(self) -> eyre::Result<Poller> {
+        let sleep_time = self
+            .sleep_time
+            .ok_or(eyre::eyre!("no sleep time provided"))?;
+        Ok(Poller {
+            client: self.client,
+            prs: Vec::new(),
+            sleep_time: Duration::from_secs(sleep_time),
+        })
+    }
+}
+
 struct Poller {
     client: GitHubClient,
     prs: Vec<Pr>,
+    sleep_time: Duration,
 }
 
 impl Poller {
-    fn new(client: GitHubClient) -> Self {
+    fn builder(client: GitHubClient) -> PollerBuilder {
         // TODO: enumerate workflows on startup and use cache?
-        Self {
+        PollerBuilder {
             client,
-            prs: Vec::new(),
+            sleep_time: None,
         }
     }
 
@@ -63,99 +96,99 @@ impl Poller {
     }
 
     fn start(&mut self) -> eyre::Result<()> {
-        for pr in &mut self.prs {
-            let span = tracing::debug_span!("", pr = ?pr.number);
-            let _guard = span.enter();
+        loop {
+            for pr in &mut self.prs {
+                let span = tracing::debug_span!("", pr = ?pr.number);
+                let _guard = span.enter();
 
-            // fetch pr branch
-            tracing::debug!("fetching pr info");
-            let pr_info: GetPullRequestResponse = self
-                .client
-                .get(
-                    format!(
-                        "https://api.github.com/repos/{}/{}/pulls/{}",
-                        &pr.owner, &pr.repo, &pr.number
-                    ),
-                    None::<()>,
-                )
-                .wrap_err("fetching branch info")?;
+                // fetch pr branch
+                tracing::debug!("fetching pr info");
+                let pr_info: GetPullRequestResponse = self
+                    .client
+                    .get(
+                        format!(
+                            "https://api.github.com/repos/{}/{}/pulls/{}",
+                            &pr.owner, &pr.repo, &pr.number
+                        ),
+                        None::<()>,
+                    )
+                    .wrap_err("fetching branch info")?;
 
-            // fetch workflow runs for branch
-            tracing::debug!("fetching workflow runs");
-            let GetWorkflowRunsResponse { mut workflow_runs } = self
-                .client
-                .get(
-                    format!(
-                        "https://api.github.com/repos/{}/{}/actions/workflows/{}/runs",
-                        &pr.owner, &pr.repo, EXT_TESTS_NUMBER
-                    ),
-                    Some(GetWorkflowRunsQueryArgs {
-                        branch: pr_info.head.branch.clone(),
-                    }),
-                )
-                .wrap_err("fetching workflow runs")?;
-            workflow_runs.sort_by_key(|k| k.run_number);
-            let Some(run) = workflow_runs.pop() else {
-                // TODO
-                eyre::bail!("no workflow runs found");
-            };
+                // fetch workflow runs for branch
+                tracing::debug!("fetching workflow runs");
+                let GetWorkflowRunsResponse { mut workflow_runs } = self
+                    .client
+                    .get(
+                        format!(
+                            "https://api.github.com/repos/{}/{}/actions/workflows/{}/runs",
+                            &pr.owner, &pr.repo, EXT_TESTS_NUMBER
+                        ),
+                        Some(GetWorkflowRunsQueryArgs {
+                            branch: pr_info.head.branch.clone(),
+                        }),
+                    )
+                    .wrap_err("fetching workflow runs")?;
+                workflow_runs.sort_by_key(|k| k.run_number);
+                let Some(run) = workflow_runs.pop() else {
+                    // TODO
+                    eyre::bail!("no workflow runs found");
+                };
 
-            tracing::debug!(run_id = %run.id, "got latest run");
+                tracing::debug!(run_id = %run.id, "got latest run");
 
-            // TODO: only if the run is in progress
-            // get run jobs
-            tracing::debug!("fetching jobs for run");
-            let GetRunJobsResponse { jobs } = self
-                .client
-                .get(
-                    format!(
-                        "https://api.github.com/repos/{}/{}/actions/runs/{}/jobs",
-                        pr.owner, pr.repo, run.id
-                    ),
-                    None::<()>,
-                )
-                .wrap_err("fetching run jobs")?;
+                // TODO: only if the run is in progress
+                // get run jobs
+                tracing::debug!("fetching jobs for run");
+                let GetRunJobsResponse { jobs } = self
+                    .client
+                    .get(
+                        format!(
+                            "https://api.github.com/repos/{}/{}/actions/runs/{}/jobs",
+                            pr.owner, pr.repo, run.id
+                        ),
+                        None::<()>,
+                    )
+                    .wrap_err("fetching run jobs")?;
 
-            // DEBUG
-            let mut f = std::fs::File::create("in-progress-jobs.json").unwrap();
-            if let Err(e) = serde_json::to_writer_pretty(&mut f, &jobs) {
-                tracing::warn!(error = ?e, "error saving in-progress job JSON state");
-            }
-
-            tracing::debug!("updating PR state");
-            match run.status.as_str() {
-                "completed" => match run.conclusion.as_deref() {
-                    Some("failure") => {
-                        tracing::debug!(before = ?pr.status, after = ?Status::Failed, "updating status");
-                        pr.status = Status::Failed;
-                    }
-                    Some("success") => {
-                        tracing::debug!(before = ?pr.status, after = ?Status::Succeeded, "updating status");
-                        pr.status = Status::Succeeded;
-                    }
-                    other => todo!(
-                        "unhandled combination of status: completed and conclusion: {other:?}"
-                    ),
-                },
-                "queued" => {
-                    let new_status = Status::Queued;
-                    tracing::debug!(before = ?pr.status, after = ?new_status, "updating status");
-                    pr.status = new_status;
+                // DEBUG
+                let mut f = std::fs::File::create("in-progress-jobs.json").unwrap();
+                if let Err(e) = serde_json::to_writer_pretty(&mut f, &jobs) {
+                    tracing::warn!(error = ?e, "error saving in-progress job JSON state");
                 }
-                "in_progress" => {
-                    // TODO: work out completion percentage
-                    let new_status = Status::InProgress(0.0);
-                    tracing::debug!(before = ?pr.status, after = ?new_status, "updating status");
-                    pr.status = new_status;
+
+                tracing::debug!("updating PR state");
+                match run.status.as_str() {
+                    "completed" => match run.conclusion.as_deref() {
+                        Some("failure") => {
+                            tracing::debug!(before = ?pr.status, after = ?Status::Failed, "updating status");
+                            pr.status = Status::Failed;
+                        }
+                        Some("success") => {
+                            tracing::debug!(before = ?pr.status, after = ?Status::Succeeded, "updating status");
+                            pr.status = Status::Succeeded;
+                        }
+                        other => todo!(
+                            "unhandled combination of status: completed and conclusion: {other:?}"
+                        ),
+                    },
+                    "queued" => {
+                        let new_status = Status::Queued;
+                        tracing::debug!(before = ?pr.status, after = ?new_status, "updating status");
+                        pr.status = new_status;
+                    }
+                    "in_progress" => {
+                        // TODO: work out completion percentage
+                        let progress = calculate_progress(&jobs).unwrap_or(0.0);
+                        let new_status = Status::InProgress(progress);
+                        tracing::debug!(before = ?pr.status, after = ?new_status, "updating status");
+                        pr.status = new_status;
+                    }
+                    other => todo!("unhandled status: {other}"),
                 }
-                other => todo!("unhandled status: {other}"),
+                tracing::debug!("finished");
             }
-            tracing::debug!("finished");
+            std::thread::sleep(self.sleep_time);
         }
-
-        std::process::exit(0);
-
-        // std::thread::sleep(Duration::from_secs(1));
     }
 }
 
@@ -163,8 +196,13 @@ fn main() -> eyre::Result<()> {
     color_eyre::install().unwrap();
     tracing_subscriber::fmt::init();
 
+    let args = Args::parse();
+
     let client = GitHubClient::from_env().unwrap();
-    let mut poller = Poller::new(client);
+    let mut poller = Poller::builder(client)
+        .with_sleep_time(args.sleep_time)
+        .build()
+        .wrap_err("invalid poller configuration")?;
     poller.add(PrDescription {
         number: 3375,
         repo: "localstack-ext".to_string(),
@@ -236,13 +274,39 @@ fn main() -> eyre::Result<()> {
 // }
 
 fn calculate_progress(jobs: &[RunJob]) -> eyre::Result<f32> {
-    todo!()
+    let mut n_steps_total = 0;
+    let mut completed_jobs = 0.0f32;
+    for job in jobs {
+        let n_steps = job.steps.len();
+        if job.status == "completed" {
+            n_steps_total += n_steps;
+            completed_jobs += 1.0;
+            continue;
+        }
+
+        for step in &job.steps {
+            n_steps_total += 1;
+            if step.status == "completed" {
+                completed_jobs += 1f32 / (n_steps as f32);
+            }
+        }
+    }
+    // TODO: fallable cast
+    Ok(completed_jobs / (n_steps_total as f32))
 }
 
 #[cfg(test)]
 mod tests {
+    use approx::assert_abs_diff_eq;
+
     use super::calculate_progress;
+    use super::GetRunJobsResponse;
 
     #[test]
-    fn from_example() {}
+    fn from_example() {
+        let s = std::fs::read_to_string("testdata/in-progress-jobs.json").unwrap();
+        let GetRunJobsResponse { jobs } = serde_json::from_str(&s).unwrap();
+        let progress = calculate_progress(&jobs).unwrap();
+        assert_abs_diff_eq!(progress, 0.125, epsilon = 0.001);
+    }
 }
